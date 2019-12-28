008380255b2fa7a06606f9e9a7719df808cf0e87
gizmore <gizmore@wechall.net>
2019-12-28 01:50:15 +0100

    6.10-r1044_fixes

--- Module_MailGPG.php
+++ Module_MailGPG.php
@@ -6,7 +6,6 @@ use GDO\Core\GDO_Module;
 final class Module_MailGPG extends GDO_Module
 {
 	public function getUserSettingsURL() { return href('MailGPG', 'Encryption'); }
-	public function getClasses() { return ['GDO\GPGMail\GDO_PublicKey']; }
+	public function getClasses() { return ['GDO\MailGPG\GDO_PublicKey']; }
 	public function onLoadLanguage() { $this->loadLanguage('lang/mailgpg'); }
-	
 }
